6fc38a865271fe0fe1e47aebad563ddebcf4a0c4
Hi5 <72061696+high5technologies@users.noreply.github.com>
2022-01-30 10:07:23 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "project_id" {
-  type        = string
-  description = "The Google Cloud Project Id"
-}
+#variable "project_id" {
+#  type        = string
+#  description = "The Google Cloud Project Id"
+#}
 
-variable "region" {
-  type    = string
-  default = "europe-west2"
-}
+#variable "region" {
+#  type    = string
+#  default = "europe-west2"
+#}
