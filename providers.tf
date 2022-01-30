a3ce578d042a343259f73259fcc0b6c7e4d607f9
Hi5 <72061696+high5technologies@users.noreply.github.com>
2022-01-30 09:24:51 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,8 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  #project = var.project_id
+  project = "sports-analytics-dev"
+  #region  = var.region
+  region = "us-central1" 
 }
 
 #terraform {
