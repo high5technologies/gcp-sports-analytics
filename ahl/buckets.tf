3a67f8ca3ab8f250c047a40931e91189e99223d0
High 5 <high5technologies@gmail.com>
2022-02-02 23:08:04 -0600

    bucket change

--- ahl/buckets.tf
+++ ahl/buckets.tf
@@ -4,9 +4,4 @@ resource "google_storage_bucket" "test-bucket-1" {
   force_destroy = true
 }
 
-resource "google_storage_bucket" "test-bucket-3" {
-  name = "test-bucket-github-3"
-  location      = "US"
-  force_destroy = true
-}
 
