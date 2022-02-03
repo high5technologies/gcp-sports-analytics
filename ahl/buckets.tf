663f1f9a61e84fbde7c1f74b409d6dbe81d9af65
High 5 <high5technologies@gmail.com>
2022-02-02 23:34:04 -0600

    trigger change

--- ahl/buckets.tf
+++ ahl/buckets.tf
@@ -4,3 +4,8 @@ resource "google_storage_bucket" "test-bucket-1" {
   force_destroy = true
 }
 
+resource "google_storage_bucket" "test-bucket-3" {
+  name = "test-bucket-github-3"
+  location      = "US"
+  force_destroy = true
+}
