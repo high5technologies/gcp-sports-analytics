4cac5b38c3f1f7666cc213702c9b02cfaff79b4f
High 5 <high5technologies@gmail.com>
2022-02-03 18:33:18 -0600

    trigger code

--- hockey-ahl/buckets.tf
+++ hockey-ahl/buckets.tf
@@ -3,3 +3,10 @@ resource "google_storage_bucket" "test-bucket-1" {
   location      = "US"
   force_destroy = true
 }
+
+resource "google_storage_bucket" "test-bucket-2" {
+  name = "test-bucket-github-2"
+  location      = "US"
+  force_destroy = true
+}
+
