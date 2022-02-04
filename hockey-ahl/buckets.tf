837ca92910e7151a2755e8734274b95198dbd358
High 5 <high5technologies@gmail.com>
2022-02-03 18:40:15 -0600

    mult branch trigger

--- hockey-ahl/buckets.tf
+++ hockey-ahl/buckets.tf
@@ -4,8 +4,3 @@ resource "google_storage_bucket" "test-bucket-1" {
   force_destroy = true
 }
 
-resource "google_storage_bucket" "test-bucket-2" {
-  name = "test-bucket-github-2"
-  location      = "US"
-  force_destroy = true
-}
