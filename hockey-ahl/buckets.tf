1693afb9fbf09aad22a958e494aaae67b308bd05
High 5 <high5technologies@gmail.com>
2022-02-03 18:45:12 -0600

    test google key

--- hockey-ahl/buckets.tf
+++ hockey-ahl/buckets.tf
@@ -4,3 +4,8 @@ resource "google_storage_bucket" "test-bucket-1" {
   force_destroy = true
 }
 
+resource "google_storage_bucket" "test-bucket-11" {
+  name = "test-bucket-github-11"
+  location      = "US"
+  force_destroy = true
+}
