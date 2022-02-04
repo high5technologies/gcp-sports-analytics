ac460cd91f45fca09aa587317866e8e3457e9c1f
High 5 <high5technologies@gmail.com>
2022-02-03 18:09:56 -0600

    config clean up

--- hockey-ahl/buckets.tf
+++ hockey-ahl/buckets.tf
@@ -3,11 +3,3 @@ resource "google_storage_bucket" "test-bucket-1" {
   location      = "US"
   force_destroy = true
 }
-
-resource "google_storage_bucket" "test-bucket-3" {
-  name = "test-bucket-github-3"
-  location      = "US"
-  force_destroy = true
-}
-
-
