5e010a3782000e26aabcd018b1132b19d27f3acd
High 5 <high5technologies@gmail.com>
2022-02-04 15:32:44 -0600

    pubsub correct name

--- common/pubsub.tf
+++ common/pubsub.tf
@@ -1,11 +1,11 @@
 # Google Pubsup Topic: BigQuery Replication
 resource "google_pubsub_topic" "bigquery_replication_topic" {
-  name = "bigquery_replication_topic_"
+  name = "bigquery_replication_topic"
 }
 
 # Google Pubsup Topic: BigQuery Replication
 resource "google_pubsub_topic" "error_log_topic" {
-  name = "error_log_topic_"
+  name = "error_log_topic"
 }
 
 # Test for function 
