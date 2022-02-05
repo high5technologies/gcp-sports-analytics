d0929d1378fad0847f35c58d02eedbc5a9d22b20
High 5 <high5technologies@gmail.com>
2022-02-05 12:45:20 -0600

    http trigger required

--- common/functions.tf
+++ common/functions.tf
@@ -41,7 +41,7 @@ module "function_SportsAnalytics_Common_BigQuery_TestLoad" {
   function_region = var.gcp_region
   function_runtime = "python39"
   function_available_memory_mb = 128
-  function_trigger_http = false
+  function_trigger_http = true
   #function_event_trigger_type = "google.pubsub.topic.publish"
   #function_event_trigger_resource = google_pubsub_topic.bigquery_replication_topic.name
 }
