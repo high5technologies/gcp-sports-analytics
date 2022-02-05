a99dd62b7c77c99a8627e575f4fa6f2202feb52d
High 5 <high5technologies@gmail.com>
2022-02-05 12:37:39 -0600

    no trigger on test job

--- common/functions.tf
+++ common/functions.tf
@@ -41,9 +41,9 @@ module "function_SportsAnalytics_Common_BigQuery_TestLoad" {
   function_region = var.gcp_region
   function_runtime = "python39"
   function_available_memory_mb = 128
-  #function_trigger_http = null
+  function_trigger_http = false
   #function_event_trigger_type = "google.pubsub.topic.publish"
-  function_event_trigger_resource = google_pubsub_topic.bigquery_replication_topic.name
+  #function_event_trigger_resource = google_pubsub_topic.bigquery_replication_topic.name
 }
 
 
