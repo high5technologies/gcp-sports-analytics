d53311666ef1bc01196447977e5236119a3ca82d
High 5 <high5technologies@gmail.com>
2022-02-10 11:16:50 -0600

    bucket names

--- common/buckets.tf
+++ common/buckets.tf
@@ -1,5 +1,5 @@
 # Create bucket to deploy code to
-resource "google_storage_bucket" "deploy_bucket_common" {
+resource "google_storage_bucket" "deploy_bucket" {
   name = "function_deploy_common_${var.gcp_project_id}"
   location      = "US"
   #force_destroy = true
--- hockey-ahl/buckets.tf
+++ hockey-ahl/buckets.tf
@@ -1,5 +1,5 @@
 # Create bucket to deploy code to
-resource "google_storage_bucket" "deploy_bucket_hockey_ahl" {
+resource "google_storage_bucket" "deploy_bucket" {
   name = "function_deploy_hockey_ahl_${var.gcp_project_id}"
   location      = "US"
   #force_destroy = true
