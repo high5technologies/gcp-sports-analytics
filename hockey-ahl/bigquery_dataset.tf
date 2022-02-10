243c615316b2ddbcf1599b70de99d1c2ff7602d3
High 5 <high5technologies@gmail.com>
2022-02-10 10:18:46 -0600

    Updated dataset name

--- hockey-ahl/bigquery_dataset.tf
+++ hockey-ahl/bigquery_dataset.tf
@@ -3,10 +3,10 @@
 # Google BigQuery:  Data Set
 ##########################################################################################
 
-resource "google_bigquery_dataset" "bq_dataset_common" {
-  dataset_id                  = "common"
-  friendly_name               = "common"
-  description                 = "Data set for common"
+resource "google_bigquery_dataset" "bq_dataset_ahl" {
+  dataset_id                  = "ahl"
+  friendly_name               = "ahl"
+  description                 = "Data set for ahl"
   location                    = "US"
   #default_table_expiration_ms = 3600000
 
