423a754618d700fe4861ac032ca7edf5835a0c1d
High 5 <high5technologies@gmail.com>
2022-02-14 11:24:38 -0600

    delete protection false

--- hockey-ahl/bigquery_tables.tf
+++ hockey-ahl/bigquery_tables.tf
@@ -3,6 +3,7 @@ resource "google_bigquery_table" "bq_table_ahl_raw_hockeytech_roster" {
     dataset_id = google_bigquery_dataset.bq_dataset_ahl.dataset_id
     table_id   = "raw_hockeytech_roster"
     schema = file("${path.module}/bigquery/tables/ahl_raw_hockeytech_roster.json")
+    deletion_protection=false
     labels = {
         env = var.env
         project = "sports_analytics"
@@ -23,6 +24,7 @@ resource "google_bigquery_table" "bq_table_ahl_raw_hockeytech_coach" {
     dataset_id = google_bigquery_dataset.bq_dataset_ahl.dataset_id
     table_id   = "raw_hockeytech_coach" 
     schema = file("${path.module}/bigquery/tables/ahl_raw_hockeytech_coach.json")
+    deletion_protection=false
     labels = {
         env = var.env
         project = "sports_analytics"
@@ -40,6 +42,7 @@ resource "google_bigquery_table" "bq_table_ahl_raw_hockeytech_game" {
     dataset_id = google_bigquery_dataset.bq_dataset_ahl.dataset_id
     table_id   = "raw_hockeytech_game" 
     schema = file("${path.module}/bigquery/tables/ahl_raw_hockeytech_game.json")
+    deletion_protection=false
     labels = {
         env = var.env
         project = "sports_analytics"
@@ -59,6 +62,7 @@ resource "google_bigquery_table" "bq_table_ahl_raw_hockeytech_gamelog" {
     dataset_id = google_bigquery_dataset.bq_dataset_ahl.dataset_id
     table_id   = "raw_hockeytech_gamelog" 
     schema = file("${path.module}/bigquery/tables/ahl_raw_hockeytech_gamelog.json")
+    deletion_protection=false
     labels = {
         env = var.env
         project = "sports_analytics"
@@ -76,6 +80,7 @@ resource "google_bigquery_table" "bq_table_ahl_raw_hockeytech_goaliebox" {
     dataset_id = google_bigquery_dataset.bq_dataset_ahl.dataset_id
     table_id   = "raw_hockeytech_goaliebox" 
     schema = file("${path.module}/bigquery/tables/ahl_raw_hockeytech_goaliebox.json")
+    deletion_protection=false
     labels = {
         env = var.env
         project = "sports_analytics"
@@ -93,6 +98,7 @@ resource "google_bigquery_table" "bq_table_ahl_raw_hockeytech_goalielog" {
     dataset_id = google_bigquery_dataset.bq_dataset_ahl.dataset_id
     table_id   = "raw_hockeytech_goalielog" 
     schema = file("${path.module}/bigquery/tables/ahl_raw_hockeytech_goalielog.json")
+    deletion_protection=false
     labels = {
         env = var.env
         project = "sports_analytics"
@@ -110,6 +116,7 @@ resource "google_bigquery_table" "bq_table_ahl_raw_hockeytech_mvp" {
     dataset_id = google_bigquery_dataset.bq_dataset_ahl.dataset_id
     table_id   = "raw_hockeytech_mvp" 
     schema = file("${path.module}/bigquery/tables/ahl_raw_hockeytech_mvp.json")
+    deletion_protection=false
     labels = {
         env = var.env
         project = "sports_analytics"
@@ -127,6 +134,7 @@ resource "google_bigquery_table" "bq_table_ahl_raw_hockeytech_ref" {
     dataset_id = google_bigquery_dataset.bq_dataset_ahl.dataset_id
     table_id   = "raw_hockeytech_ref" 
     schema = file("${path.module}/bigquery/tables/ahl_raw_hockeytech_ref.json")
+    deletion_protection=false
     labels = {
         env = var.env
         project = "sports_analytics"
@@ -144,6 +152,7 @@ resource "google_bigquery_table" "bq_table_ahl_raw_hockeytech_skaterbox" {
     dataset_id = google_bigquery_dataset.bq_dataset_ahl.dataset_id
     table_id   = "raw_hockeytech_skaterbox" 
     schema = file("${path.module}/bigquery/tables/ahl_raw_hockeytech_skaterbox.json")
+    deletion_protection=false
     labels = {
         env = var.env
         project = "sports_analytics"
