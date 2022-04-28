f007e6c64fcc0c9c7b1fc7ce1a4d56d5704e9fea
Hi5 <72061696+high5technologies@users.noreply.github.com>
2022-04-28 18:55:36 -0500

    bq ref tables

--- basketball-nba/bigquery_tables.tf
+++ basketball-nba/bigquery_tables.tf
@@ -1,3 +1,6 @@
+##########################################################################################
+# Google BigQuery:  raw
+##########################################################################################
 
 resource "google_bigquery_table" "bq_table_nba_raw_nbacom_game" {
     dataset_id = google_bigquery_dataset.bq_dataset_nba.dataset_id
@@ -256,5 +259,89 @@ resource "google_bigquery_table" "bq_table_nba_raw_espn_playbyplay" {
         expiration_ms            = null   
     }
     clustering = ["game_date"]
+    deletion_protection = true
+}
+
+##########################################################################################
+# Google BigQuery:  reference
+##########################################################################################
+
+resource "google_bigquery_table" "nba_ref_team" {
+    dataset_id = google_bigquery_dataset.bq_dataset_nba.dataset_id
+    table_id   = "ref_team"
+    description = "google sheet that provides meta data to basketball reference provides.  Google sheet is manually maintained.  This table should be used in LEFT JOINs to prevent data loss if data does not exist in google sheet reference values"
+    schema = file("${path.module}/bigquery/tables/transform/nba_ref_team.json")
+    labels = {
+        env = var.env
+        project = "sports_analytics"
+        league = "nba"
+    }
+    external_data_configuration {
+        autodetect    = false
+        source_format = "GOOGLE_SHEETS"
+
+        google_sheets_options {
+            skip_leading_rows = 1
+            range = "reference!A:F"
+        }
+
+        source_uris = [
+            "https://docs.google.com/spreadsheets/d/1nHPWY-017onYBgtC7E_enqmCLDi-oCnr5cyHk8enPYM/"
+        ]
+    }
+    deletion_protection = true
+}
+
+resource "google_bigquery_table" "nba_ref_538_spread_probability" {
+    dataset_id = google_bigquery_dataset.bq_dataset_nba.dataset_id
+    table_id   = "ref_538_spread_probability"
+    description = "google sheet on historical data for the 538 probability to spread"
+    schema = file("${path.module}/bigquery/tables/transform/nba_ref_538_spread_probability.json")
+    labels = {
+        env = var.env
+        project = "sports_analytics"
+        league = "nba"
+    }
+    external_data_configuration {
+        autodetect    = false
+        source_format = "GOOGLE_SHEETS"
+
+        google_sheets_options {
+            skip_leading_rows = 1
+            range = "prob!A:B"
+        }
+
+        source_uris = [
+            "https://docs.google.com/spreadsheets/d/1MA0MExULs2K826hq6ICseargZMMU6CbrMi_1JHRjtPU/"
+        ]
+    }
+    deletion_protection = true
+}
+
+
+
+resource "google_bigquery_table" "nba_ref_team_alias" {
+    dataset_id = google_bigquery_dataset.bq_dataset_nba.dataset_id
+    table_id   = "ref_team_alias"
+    description = "google sheet that provides aliases for different source systems that have may different team aliases in their data sets.  Google sheet is manually maintained.  This table should be used in LEFT JOINs to prevent data loss if data does not exist in google sheet reference values"
+    schema = file("${path.module}/bigquery/tables/transform/nba_ref_team_alias.json")
+    labels = {
+        env = var.env
+        project = "sports_analytics"
+        league = "nba"
+    }
+    external_data_configuration {
+        autodetect    = false
+        source_format = "GOOGLE_SHEETS"
+
+        google_sheets_options {
+            skip_leading_rows = 1
+            range = "alias!A:B"
+        }
+
+        source_uris = [
+            "https://docs.google.com/spreadsheets/d/1nHPWY-017onYBgtC7E_enqmCLDi-oCnr5cyHk8enPYM/"
+        ]
+    }
     deletion_protection = true
 }
