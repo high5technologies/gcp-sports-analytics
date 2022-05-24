0a66f8e97f7c07b36ce59dc12ffd08f6175c987b
DataFiveLearn <datafivelearn@gmail.com>
2022-05-24 00:15:59 -0500

    updated name

--- api/project-service.tf
+++ api/project-service.tf
@@ -1,4 +1,4 @@
-resource "google_project_service" "project" {
+resource "google_project_service" "project_apigateway" {
   project = var.gcp_project_id
   #service = "iam.googleapis.com"
   service = "apigateway.googleapis.com"
