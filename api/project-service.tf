1dc6676cb3f5da80314c4ff339384754823deed3
DataFiveLearn <datafivelearn@gmail.com>
2022-05-24 00:11:09 -0500

    project

--- api/project-service.tf
+++ api/project-service.tf
@@ -1,15 +1,18 @@
-resource "google_project_service" "project_apigateway" {
+resource "google_project_service" "project" {
   project = var.gcp_project_id
   #service = "iam.googleapis.com"
   service = "apigateway.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "project_servicemanagement" {
   project = var.gcp_project_id
   service = "servicemanagement.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "project_servicecontrol" {
   project = var.gcp_project_id
   service = "servicecontrol.googleapis.com"
+  disable_on_destroy = false
 }
