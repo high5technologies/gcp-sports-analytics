78e07f1c06177dd01abd21d62be0cb5b8dd230b4
DataFiveLearn <datafivelearn@gmail.com>
2022-05-24 00:29:31 -0500

    SA & API

--- api/apigateway.tf
+++ api/apigateway.tf
@@ -5,4 +5,13 @@ locals {
   display_name             = "API for Sports Analytics Consjmption"
 }
 
+resource "google_api_gateway_api" "api_sports_analytics" {
+  provider     = google-beta
+  #api_id       = local.api_gateway_container_id
+  api_id       = local.api_id
+  project      = var.gcp_project_id
+  display_name = local.display_name
+
+  depends_on = [google_project_service.project_apigateway,google_project_service.project_servicemanagement,google_project_service.project_servicecontrol]
+}
 
---
+++ api/iam.tf
@@ -0,0 +1,10 @@
+resource "google_service_account" "sa_sports_analytics" {
+  account_id   = "sa-sports-analytics-api"
+  display_name = "Service Account for Sports Analytics API to call backend resources"
+}
+
+resource "google_service_account_iam_member" "iam_sa_sports_analytics_function_invoker" {
+  service_account_id = data.google_compute_default_service_account.default.name
+  role               = "roles/cloudfunctions.invoker"
+  member             = "serviceAccount:${google_service_account.sa_sports_analytics.email}"
+}
