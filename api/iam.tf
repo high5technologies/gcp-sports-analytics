93e0312a19eff54c866c61f90cf84ee11f15fa04
DataFiveLearn <datafivelearn@gmail.com>
2022-05-24 01:02:41 -0500

    try again on SA

--- api/iam.tf
+++ api/iam.tf
@@ -2,12 +2,16 @@ resource "google_service_account" "sa_sports_analytics" {
   account_id   = "sa-sports-analytics-api"
   display_name = "Service Account for Sports Analytics API to call backend resources"
 }
-
-resource "google_service_account_iam_member" "iam_sa_sports_analytics_function_invoker" {
-  #service_account_id = google_service_account.sa_sports_analytics.name
-  role               = "roles/cloudfunctions.invoker"
-  member             = "serviceAccount:${google_service_account.sa_sports_analytics.email}"
+resource "google_project_iam_member" "iam_sa_sports_analytics_function_invoker" {
+  project = var.gcp_project_id
+  role    = "roles/cloudfunctions.invoker"
+  members = ["serviceAccount:${google_service_account.sa_sports_analytics.email}"]
 }
+#resource "google_service_account_iam_member" "iam_sa_sports_analytics_function_invoker" {
+#  #service_account_id = google_service_account.sa_sports_analytics.name
+#  role               = "roles/cloudfunctions.invoker"
+#  member             = "serviceAccount:${google_service_account.sa_sports_analytics.email}"
+#}
 #resource "google_service_account" "sa-name" {
 #  account_id = "sa-name"
 #  display_name = "SA"
