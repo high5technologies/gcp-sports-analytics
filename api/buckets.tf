03266541d07e1845050c4f402e8daceaaf9fd3de
high5technologies <high5technologies@gmail.com>
2022-09-05 22:48:54 -0500

    moving API to backend repo

--- api/buckets.tf
+++
@@ -1,6 +0,0 @@
-# Create bucket to deploy code to
-resource "google_storage_bucket" "deploy_bucket" {
-  name = "function_deploy_api_${var.gcp_project_id}"
-  location      = "US"
-  #force_destroy = true
-}
--- api/env-config/dev.tfvars
+++
@@ -1,3 +0,0 @@
-gcp_project_id = "sports-analytics-dev"
-gcp_region = "us-central1"
-env = "dev"
